a17fbba3baa17cf80c49739f5a4236e408016bbe
Samajas <samuelpashaginting@student.telkomuniversity.ac.id>
2024-07-28 18:31:19 +0700

    Merge fitur dari branch frontend

--- TestCommitPush2.py
+++ TestCommitPush2.py
@@ -5,5 +5,5 @@ userage = input("How old are you? : ")
 usergender = input("What is your gender? : ")
 useradd = input("Would you tell me where you live? : ")
 
-print("Hello, " + username + ", Nice to meet you\nI see that you are "+userage+" years old.\nAh, you are a "+usergender+", and you live at "+useradd+" , it's nice to know you.")
+print("Hello, " + username + ", Nice to meet you\nI see that you are "+userage+" years old.\nAh, you are a "+usergender+", and you live at "+useradd+", it's nice to know you.")
 
